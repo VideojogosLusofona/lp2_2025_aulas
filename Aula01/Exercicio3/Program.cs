030be12d4430ca1d311f01752199f52d427d77eb
Nuno Fachada <nuno.fachada@ulusofona.pt>
2025-09-20 19:11:36 +0100

    Add aula 1 files

---
+++ Aula01/Exercicio3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercicio3
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
