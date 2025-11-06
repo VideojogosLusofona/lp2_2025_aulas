a82453328e1b8ec702d2ebbb74584c25ecc9803b
Nuno Fachada <nuno.fachada@ulusofona.pt>
2025-11-06 17:01:28 +0000

    Adicionar exercícios resolvidos da aula 9

---
+++ Aula09/Exercicio1/Assets/Scripts/RotateBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class RotateBehaviour : MonoBehaviour, IAnimateBehaviour
+{
+    [SerializeField]
+    private float rotateSpeed = 20f;
+
+    // Update is called once per frame
+    public void Animate()
+    {
+        Vector3 rotator = rotateSpeed * Time.deltaTime * Vector3.one;
+        transform.Rotate(rotator);
+    }
+}
---
+++ Aula09/Exercicio1/Assets/Scripts/ScaleBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ScaleBehaviour : MonoBehaviour, IAnimateBehaviour
+{
+    [SerializeField]
+    private float scaleSpeed = 3f;
+
+    private Vector3 minScale = new Vector3(0.1f, 0.1f, 0.1f);
+    private float angle = 0.0f;
+
+    public void Animate()
+    {
+        transform.localScale =
+            minScale + Mathf.Abs(Mathf.Cos(angle)) * Vector3.one;
+        angle += scaleSpeed * Time.deltaTime;
+    }
+}
